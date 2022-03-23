92b550ba92c7dc1071b1ea51bdf4b5ac40e8bdea
mgolcu00 <mgolcu00@gmail.com>
2022-03-23 14:32:51 +0300

    init

---
+++ index.js
@@ -0,0 +1,74 @@
+import axios from "axios";
+import { v4 as uuid } from "uuid";
+
+export class RealtimeDatabaseReferance {
+    url = 'http://localhost:8000/api/realtime/';
+    constructor(ref) {
+        this.ref = ref;
+    }
+    sse = {}
+    session_id = null;
+    listen(callback) {
+        this.session_id = uuid();
+        const sse = new EventSource([this.url + 'listen?id=' + this.session_id], { withCredentials: true });
+        this.sse = sse;
+        this.sse.onmessage = (event) => {
+            const data = JSON.parse(event.data);
+            console.log('[INFO](event) : ', event);
+            let r1 = this.ref
+            if (r1 == '*') {
+                callback(null, data);
+            } else if (r1.indexOf('.') > -1) {
+                let data2 = r1.split('.').reduce((o, i) => o[i], data);
+                callback(null, data2);
+            }
+            else if (data[r1]) {
+                callback(null, data[r1]);
+            }
+
+        }
+        this.sse.onerror = (error) => {
+            this.sse.close();
+            callback(error, null);
+        }
+        this.sse.addEventListener('open', (event) => {
+            console.log('OPEN');
+        });
+        this.sse.addEventListener('close', (event) => {
+            console.log('CLOSE');
+        });
+    }
+
+
+
+    close() {
+        axios.post(this.url + 'stop', { id: this.session_id })
+        this.sse.close();
+    }
+
+
+    push(data) {
+        return axios.post(this.url + 'put', { ref: this.ref, data: data });
+    }
+
+}
+export class RealtimeDatabaseClient {
+    referances = [];
+    constructor() {
+    }
+
+    ref(ref) {
+        if (this.referances.find(r => r.ref == ref)) {
+            return this.referances.find(r => r.ref == ref);
+        }
+        const r = new RealtimeDatabaseReferance(ref);
+        this.referances.push(r);
+        return r;
+    }
+    close() {
+        this.referances.forEach(ref => {
+            console.log('close', ref);
+            ref.close();
+        })
+    }
+}
